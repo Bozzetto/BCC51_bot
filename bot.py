053574f9a8ce7c719aad41967d1c8ae7d98b528f
Bozzetto <leobozzetto17@gmail.com>
2021-06-28 16:24:53 +0000

    Adicionei conexao ao banco de dados MariaDB

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
+#Importacao de modulos
 import telebot
 import datetime
 import threading
-import mysql.connector
+import mariadb
+import sys
 
+#Transforma um numero em uma lista das potencias de 2 que o compoem
 def materias_number_to_lista(num):
     lista = [];
     i = 1;
@@ -14,8 +17,31 @@ def materias_number_to_lista(num):
         num = num-resto
     return lista
 
+#Transforma uma lista de potencias de 2 em um numero
 def materias_lista_to_number(lista):
     sum = 0
     for i in lista:
         sum = sum + i
     return sum
+
+def main():
+    try:
+        conn = mariadb.connect(
+            user = "anon",
+            password = "teste",
+            host = "localhost",
+            port = 3306,
+            database = "mydb"
+        )
+    except BaseException:
+        print("ok")
+        sys.exit(1)
+    except mariadb.Error as e:
+        print(f"Error connecting to Mariadb:{e}")
+        sys.exit(1)
+
+    cur = conn.cursor()
+
+
+if __name__ == '__main__':
+    main()
